d92fce156f102184cbbb4313a0c385cc4e475a40
Diluka W <dt.wj85@gmail.com>
2022-10-21 12:18:13 +0530

    created navbar and footer

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserRegistrationComponent } from './user-registration/user-registration.component';
+import { UserRegistrationComponent } from './components/user-registration/user-registration.component';
 import { FormsModule } from '@angular/forms'; //
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './sharedcomponents/navbar/navbar.component';
+import { FooterComponent } from './sharedcomponents/footer/footer.component';
+import { TrainersComponent } from './components/trainers/trainers.component';
+import { BannerComponent } from './components/home/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserRegistrationComponent
+    UserRegistrationComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    TrainersComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
